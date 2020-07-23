c53db3700517798cf4b42778cdb73c2424471802
Taila Sultana <taila.sultana@gmail.com>
2020-07-23 22:18:13 +0500

    First commit

---
+++ src/Room.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './Room.css';
+
+function Room(){
+
+const [isLit, setLit] = React.useState(false);
+const [currentTemp, setTemp] = React.useState(72);
+
+return(
+    
+   <div className = {`room ${isLit ? "lit" : "dark"}`}>
+   
+    This room is {isLit? "lit" : "dark"}
+    <br/>
+    The room temperature is {currentTemp}
+       <br/>
+       <button onClick = {()=>setLit(!isLit)}> Toggle Light</button>
+       <br/>
+       <button onClick = {()=>setLit(true)}> ON</button>
+       <br/>
+       <button onClick = {()=>setLit(false)}> OFF</button>
+       <br/>
+       <button onClick = {()=>setTemp(currentTemp+1)}> +</button>
+       
+       <button onClick = {()=>setTemp(currentTemp-1)}> -</button>
+   </div>
+);
+}
+
+export default Room;
